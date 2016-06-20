2983c593e6a34d155a217d04463f84421ac7c6e7
German Utz <germanutz@gmail.com>
2016-06-20 17:47:01 -0300

    update instalamos bootstrap via bower

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gerutz
+ * Date: 20/6/16
+ * Time: 17:17
+ */
+
+?>
+<!doctype html>
+<html lang="es">
+<head>
+    <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <title>Sprint 2 - Index</title>
+</head>
+<body>
+
+
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+</body>
+</html>
