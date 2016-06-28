69ebeee8bd7ed57fd2661ce2269c5503ace3652b
gerutz <germanutz@gmail.com>
2016-06-28 13:30:49 -0300

    update comenzamos a trabajar el archivo clase/Auth

---
+++ clases/Auth.php
@@ -0,0 +1,17 @@
+<?php
+    require_once ('usuario.php');
+    require_once('usuarioRepositorio.php');
+
+
+class Auth {
+
+    private $usuarioRepositorio;
+    private  static  $instance = null;
+
+    public static function getInstance(usuarioRepositorio $usuarioRepositorio){
+
+    }
+
+    public function existeMail(){
+    }
+}
